e0bb34e4aa222ea311b8278c59d65c709beda769
76669 <141121970+76669@users.noreply.github.com>
2023-09-27 10:49:18 +0530

    ContactObjects.java

--- AutomationExercise-master/src/main/java/pages/ContactFormObjects.java
+++ AutomationExercise-master/src/main/java/pages/ContactFormObjects.java
@@ -7,16 +7,26 @@ import org.openqa.selenium.support.PageFactory;
 
 public class ContactFormObjects {
 
-	@FindBy (xpath="//h2[text()='Get In Touch']") public WebElement GetInTch;
-	@FindBy (xpath = "//a[text()=' Contact us']") public WebElement ContactUs;
-	@FindBy (xpath = "//input[@data-qa='name']") public WebElement ContName;
-	@FindBy (xpath = "//input[@data-qa='email']") public WebElement ContEmail;
-	@FindBy (xpath = "//input[@data-qa='subject']") public WebElement ContSubj;
-	@FindBy (xpath = "//textarea[@id='message']") public WebElement ContMsg;
-	@FindBy (xpath = "//input[@name='upload_file']") public WebElement uploadFile;
-	@FindBy (xpath = "//input[@name='submit']") public WebElement submitBtn;
-	@FindBy (xpath = "(//div[contains(text(),'Success! Your details ')])[1]") public WebElement succMsg;
-	@FindBy (xpath = "//a//span[text()=' Home']") public WebElement home;
+	@FindBy (xpath="//h2[text()='Get In Touch']")
+	public WebElement getintuch;
+	@FindBy (xpath = "//a[text()=' Contact us']")
+	public WebElement contactus;
+	@FindBy (xpath = "//input[@data-qa='name']")
+	public WebElement name;
+	@FindBy (xpath = "//input[@data-qa='email']")
+	public WebElement email;
+	@FindBy (xpath = "//input[@data-qa='subject']")
+	public WebElement subject;
+	@FindBy (xpath = "//textarea[@id='message']")
+	public WebElement message;
+	@FindBy (xpath = "//input[@name='upload_file']")
+	public WebElement fileupload;
+	@FindBy (xpath = "//input[@name='submit']") 
+	public WebElement submit;
+	@FindBy (xpath = "(//div[contains(text(),'Success! Your details ')])[1]") 
+	public WebElement successmessage;
+	@FindBy (xpath = "//a//span[text()=' Home']") 
+	public WebElement home;
 	
 	WebDriver driver;
 	
