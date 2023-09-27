d2eb3738f7049a19cbbd9011a7755cd4563fa7e9
76669 <141121970+76669@users.noreply.github.com>
2023-09-27 10:49:52 +0530

    Update ContactFormObjects.java

--- AutomationExercise-master/src/main/java/pages/ContactFormObjects.java
+++ AutomationExercise-master/src/main/java/pages/ContactFormObjects.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ContactFormObjects {
+public class ContactObjects {
 
 	@FindBy (xpath="//h2[text()='Get In Touch']")
 	public WebElement getintuch;
