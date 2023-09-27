9efc42bec075cbd2751e83d988f65f7c0666513b
76669 <141121970+76669@users.noreply.github.com>
2023-09-27 13:45:18 +0530

    Update TestCasesObjects.java

--- AutomationExercise-master/src/main/java/pages/TestCasesObjects.java
+++ AutomationExercise-master/src/main/java/pages/TestCasesObjects.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class TestCasesObjects {
+public class testcase {
 
-	@FindBy (xpath="//a[text()=' Test Cases']") public WebElement TestCases;
-	@FindBy (xpath="//span[contains(text(),'Below is the list of test Cases')]") public WebElement validateTCTxt;
+	@FindBy (xpath="//a[text()=' Test Cases']")
+	public WebElement test_cases;
+	@FindBy (xpath="//span[contains(text(),'Below is the list of test Cases')]")
+	public WebElement validate_test_case;
 	
 	WebDriver driver;
 	public TestCasesObjects(WebDriver driver) {
